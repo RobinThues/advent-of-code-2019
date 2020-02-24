c6448366454fa68c33b00cda7c3421994149a51e
Robin Thüs <robin.thues@real-digital.de>
2020-02-24 13:55:44 +0100

    started work on day3

--- adventOfCode.go
+++ adventOfCode.go
@@ -2,6 +2,7 @@ package main
 
 func main() {
   day2(2)
+  day3(1)
   //loads := day1(1)
   //fmt.Println(loads)
 }
--- day3.go
+++ day3.go
@@ -1,18 +1,105 @@
 package main
 
-func readDay3Input(fileName string) {
-	// read line
-	// separate by comma
-	// first letter is direction
-	// rest to int is distance
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const(
+	DIRECTION_UP = "U"
+	DIRECTION_DOWN = "D"
+	DIRECTION_LEFT = "L"
+	DIRECTION_RIGHT = "R"
+)
+
+type Wires = []Wire
+
+type Wire struct {
+	steps []Step
 }
 
-func buildMap() {
-	// for each wire
-	// add/sub ups and downs, save max ups and max downs
-	// add/sub lefts and rights, save max right and max left
-	// create map with maxUps+Maxdowns and maxLefts+maxRights
-	// mark the middle
+type Step struct {
+	direction string
+	length int
+}
+
+func day3(part int) {
+	wires := readDay3Input("input3.txt")
+	if part == 1 {
+		buildMap(wires)
+	}
+}
+
+func readDay3Input(fileName string) Wires {
+	lines := loadInput(fileName)
+
+	var wires Wires
+
+	for _, l := range lines {
+		splittedLine := strings.Split(l, ",")
+		wire := Wire{}
+
+		for _, split := range splittedLine {
+			s, _ := strconv.Atoi(split[1:])
+			wire.steps = append(wire.steps, Step{
+				string(split[0]),
+				s,
+			})
+		}
+		wires = append(wires, wire)
+	}
+	return wires
+}
+
+type Map = [][]int
+
+func buildMap(wires Wires) Map {
+	maxUp := 0
+	maxDown := 0
+	maxLeft := 0
+	maxRight := 0
+
+	for _, wire := range wires {
+		currVertical := 0
+		currHorizontal := 0
+
+		for _, step := range wire.steps {
+			switch step.direction {
+			case DIRECTION_UP:
+				currVertical += step.length
+				if currVertical > maxUp {
+					maxUp = currVertical
+				}
+			case DIRECTION_DOWN:
+				currVertical -= step.length
+				if -currVertical > maxDown {
+					maxDown = -currVertical
+				}
+			case DIRECTION_LEFT:
+				currHorizontal -= step.length
+				if -currHorizontal > maxLeft {
+					maxLeft = -currHorizontal
+				}
+			case DIRECTION_RIGHT:
+				currHorizontal += step.length
+				if currHorizontal > maxRight {
+					maxRight = currHorizontal
+				}
+			}
+		}
+	}
+
+	fmt.Println("MaxUp:", maxUp)
+	fmt.Println("maxDown:", maxDown)
+	fmt.Println("maxLeft:", maxLeft)
+	fmt.Println("maxRight:", maxRight)
+
+	mapHorizontal := make([][]int, maxLeft+maxRight)
+	for i := range mapHorizontal {
+		mapHorizontal[i] = make([]int, maxUp+maxDown)
+	}
+	return mapHorizontal
 }
 
 func drawWiresOnMap() {
